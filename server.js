711dd3adf2c9614ae113f21d285098bc3e5678a5
Justin Agim <justinagim@gmail.com>
2023-08-12 21:52:39 -0500

    update weather and movie

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 
 const express = require('express');
 
-const data = require('./data/weather.json');
+// const data = require('./data/weather.json');
 
 const cors = require('cors');
+const { default: axios } = require('axios');
 
-require("dotenv").config()
+require("dotenv").config();
 
-console.log(data);
 
 const app = express();
 const PORT = 3001;
+
+const weatherKey = process.env.WEATHER_API_KEY
+const movieKey = process.env.MOVIES_API_KEY
 
 app.use(cors({
   origin: '*'
@@ -23,30 +26,67 @@ app.get('/', (request, response) => {
 
 
 class Forecast {
-  constructor(ForecastObj) {
-    this.valid_date = ForecastObj.valid_date;
-    this.description = ForecastObj.weather.description;
+  constructor(valid_date, description) {
+    this.valid_date = valid_date;
+    this.description = description;
   }
 }
 
 
 app.get('/weather', async (request, response, next) => {
-  let city = request.query.searchQuery;
-  console.log(city);
-  let town = data.find((p) => p.city_name === city);
-  if (!town) {
-    return next(new Error('Can\'t Find City'));
-  }
-  const extractedData = town.data.map((data) => {
-    return new Forecast(data);
-  });
+  try {
+    let { lat, lon } = request.query;
+    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherKey}&lat=${lat}&lon=${lon}`);
+    const extractedData = weatherData.data.data.map((item) => {
+      return new Forecast(item.valid_date, item.weather.description);
+    });
 
-  response.send(extractedData);
+    response.status(200).send(extractedData);
+  } catch (error) {
+    next(error);
+  }
+
+
 });
 
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
+
+class Movie {
+  constructor(title, released_on, overview) {
+    this.title = title;
+    this.released_on = released_on;
+    this.overview = overview;
+  }
+}
+
+app.get('/movie', async (request, response) => {
+  const { location } = request.query;
+
+  try {
+    const movieKey = process.env.MOVIES_API_KEY;
+    let url = `https://api.themoviedb.org/3/search/movie?api=key${movieKey}&query=${location}`;
+    const response = await axios.get(url);
+
+    const Movie = response.data.results.map((Movie) => {
+      return new Movie(Movie.title, Movie.released_on, Movie.overview);
+    });
+
+    response.json(Movie);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ error: 'An error occurred while fetching movie data' });
+  }
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
 
 
 // app.get("/weather", (request, response) => {
